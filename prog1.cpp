31f964877b1dd25dc6392ac3d6f7dcfa0f406159
haaaziq <59704449+haaaziq@users.noreply.github.com>
2020-01-09 23:42:51 +0530

    Creating a class GitHub
    class GitHub containing only show function

---
+++ prog1.cpp
@@ -0,0 +1,17 @@
+//Creating a C++ Program
+#include<iostream>
+using namespace std;
+
+class GitHub{
+    public:
+          void show(){
+            cout<<"This is a show function of class Github"<<endl;
+          }
+};
+
+int main(){
+  GitHub obj1;
+  obj1.show();
+
+return 0;
+}
